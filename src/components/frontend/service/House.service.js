40935e4f9678282452d166838a26c3cc6b42d20f
denismwangi <denniske992@gmail.com>
2021-05-12 09:09:03 +0300

    test

--- src/components/frontend/service/House.service.js
+++ src/components/frontend/service/House.service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8080/api/v1/houses';
+//const API_URL = 'http://127.0.0.1:8080/api/v1/houses';
+const API_URL = 'https://rentaapi.herokuapp.com/api/v1/houses'; 
 
 class AllHousesService{
 
